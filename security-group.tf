3d10df00fa20b5c00a7d455cc4522fa3d1706ebb
Nireeksha17 <56109493+Nireeksha17@users.noreply.github.com>
2022-11-08 12:09:37 +0530

    Add files via upload
    AWS-infrastructure

---
+++ security-group.tf
@@ -0,0 +1,47 @@
+
+resource "aws_security_group" "Group_Management1" {
+  name_prefix = "Group_Management1"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+
+    cidr_blocks = [
+      "10.0.0.0/8",
+    ]
+  }
+}
+
+resource "aws_security_group" "Group_Management2" {
+  name_prefix = "Group_Management2"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+
+    cidr_blocks = [
+      "192.168.0.0/16",
+    ]
+  }
+}
+
+resource "aws_security_group" "Master_Group" {
+  name_prefix = "Master_Group"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+
+    cidr_blocks = [
+      "10.0.0.0/8",
+      "172.16.0.0/12",
+      "192.168.0.0/16",
+    ]
+  }
+}
